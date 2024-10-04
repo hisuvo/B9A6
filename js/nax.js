48fe157bc75d4eb4b6299f9ad0e03e2f42acb49d
suvo <suvodatta72@gmail.com>
2024-10-04 23:42:49 +0600

    navigation manu done

---
+++ js/nax.js
@@ -0,0 +1,40 @@
+const nav = [
+    {
+        "logo": "ReTro",
+        "singIn": "Sing In",
+    }
+]
+
+
+// Get access navigation conatiner
+const navItems = $("nav_items");
+
+nav.map((item) => {
+    // nav logo
+    const navLogo = md("h2")
+    navLogo.classList = "text-2xl font-bold"
+    navLogo.innerText = `${item.logo}`
+    navItems.appendChild(navLogo)
+})
+
+const nav_icon = [
+    '<i class="fa-solid fa-house"></i>',
+    '<i class="fa-solid fa-user"></i>',
+    '<i class="fa-solid fa-pencil"></i>',
+    '<i class="fa-brands fa-font-awesome"></i>',
+    '<i class="fa-brands fa-phoenix-framework"></i>',
+    '<i class="fa-solid fa-phone-flip"></i>',
+    'Sing In',
+]
+
+// nav menu
+const menu = md("ul");
+menu.classList = "flex justify-center items-center gap-4";
+navItems.appendChild(menu);
+
+nav_icon.map((item) => {
+    const list = md("li")
+    console.log(item)
+    list.innerHTML = `${item === "Sing In" ? `<button class="btn px-5 btn-info rounded-full text-white"> ${item} </button>` : `<button class="btn btn-circle"> ${item} </button>`}`
+    menu.appendChild(list)
+})
---
+++ js/utilits.js
@@ -0,0 +1,3 @@
+const $ = (id) => document.getElementById(id);
+// For made new element
+const md = (id) => document.createElement(id)
